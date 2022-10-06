87aa08ec6afd0bb1147db99861378619a7499b34
Babatunde <babatundea15@gmail.com>
2022-10-06 10:06:53 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -17,10 +17,7 @@ const server = express();
 server.use(helmet());
 server.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 server.use(express.json());
